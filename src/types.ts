2b3219ce38659efd6b16fcb6d9bb39939c4eae70
Gareth Dawson <gwdawson.work@gmail.com>
2024-01-30 15:21:29 +0000

    add sql editor

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,10 @@
 import { DataSourceJsonData } from '@grafana/data';
 import { DataQuery } from '@grafana/schema';
 
-export interface MyQuery extends DataQuery {
-  queryText?: string;
-  constant: number;
+export interface YugabyteQuery extends DataQuery {
+  queryType: 'YSQL' | 'YCQL';
+  rawSql: string;
 }
-
-export const DEFAULT_QUERY: Partial<MyQuery> = {
-  constant: 6.5,
-};
 
 /**
  * These are options configured for each DataSource instance
