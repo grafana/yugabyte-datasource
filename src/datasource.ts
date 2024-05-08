809824731f6db74323cee049b76c6f00179d5bda
Gareth Dawson <email@garethdawson.xyz>
2024-05-08 16:48:17 +0100

    improved way to disable datasets (#25)

--- src/datasource.ts
+++ src/datasource.ts
@@ -18,7 +18,6 @@ import { lastValueFrom, map } from 'rxjs';
 import { YugabyteQuery, YugabyteOptions } from 'types';
 import { buildColumnQuery, buildTableQuery } from './utils/queries';
 import { completionFetchColumns, completionFetchTables, getCompletionProvider } from './utils/completion';
-import { toRawSql } from './utils/sql';
 import { AGGREGATE_FNS } from './utils/constants';
 
 export class YugabyteDataSource extends DataSourceWithBackend<YugabyteQuery, YugabyteOptions> {
@@ -122,8 +121,8 @@ export class YugabyteDataSource extends DataSourceWithBackend<YugabyteQuery, Yug
       tables: async () => await this.fetchTables(),
       fields: async (query: YugabyteQuery) => await this.fetchFields(query),
       validateQuery: async (query: YugabyteQuery) => this.validateQuery(query),
-      toRawSql: (query: YugabyteQuery) => toRawSql(query),
       getSqlCompletionProvider: () => this.getSqlCompletionProvider(this.db),
+      disableDatasets: true,
     };
   }
 
