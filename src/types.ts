9846e0d7691aa8f9ce8885a1dabfd30e06c7b148
Andreas Christou <andreas.christou@grafana.com>
2025-06-17 19:21:04 +0100

    Add PDC to config editor

--- src/types.ts
+++ src/types.ts
@@ -8,7 +8,9 @@ export interface YugabyteQuery extends SQLQuery {}
 /**
  * These are options configured for each DataSource instance
  */
-export interface YugabyteOptions extends SQLOptions {}
+export interface YugabyteOptions extends SQLOptions {
+  enableSecureSocksProxy?: boolean;
+}
 
 /**
  * Value that is used in the backend, but never sent over HTTP to the frontend
